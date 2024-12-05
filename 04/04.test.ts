875ae0faecfb3d66d9f45337fbeac52174f0d262
jyelewis <jye.lewis@propelleraero.com.au>
2024-12-05 19:20:35 +1100

    Day 4 - min

--- 04/04.test.ts
+++ 04/04.test.ts
@@ -3,6 +3,8 @@ import fs from "fs";
 import assert from "node:assert";
 import { day04a, day04b } from "./04";
 import { day04a_compact } from "./04-compact";
+// @ts-ignore
+import { day04a as day04a_min } from "./04-min.js";
 
 describe("day04", () => {
   const sampleInput = fs.readFileSync("04/example-input.txt").toString("utf-8");
@@ -35,6 +37,16 @@ describe("day04", () => {
 
   it("04a_compact input", () => {
     const answer = day04a_compact(input);
+    assert.equal(answer, 2447);
+  });
+
+  it("04a_min sample input", () => {
+    const answer = day04a_min(sampleInput);
+    assert.equal(answer, 18);
+  });
+
+  it("04a_min input", () => {
+    const answer = day04a_min(input);
     assert.equal(answer, 2447);
   });
 });
