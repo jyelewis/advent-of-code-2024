e1bddf1f9372bc9b8a096d8ced8f84c0fa6e561f
jyelewis <jye.lewis@propelleraero.com.au>
2024-12-05 13:42:21 +1100

    Day 4 - simplify

--- 04/04.ts
+++ 04/04.ts
@@ -6,15 +6,11 @@ export function day04a(input: string) {
   return range2D(grid) // count & sum how many XMAS paths originate from each cell
     .map(({ x, y }) =>
       [
-        getString(grid, x, y, 1, 0), // right
-        getString(grid, x, y, -1, 0), // left
-        getString(grid, x, y, 0, -1), // up
-        getString(grid, x, y, 0, 1), // down
-        getString(grid, x, y, 1, -1), // up-right
-        getString(grid, x, y, -1, -1), // up-left
-        getString(grid, x, y, 1, 1), // down-right
-        getString(grid, x, y, -1, 1), // down-left
-      ].count((str) => str === "XMAS"),
+        getString(grid, x, y, 1, 0), // horizontal
+        getString(grid, x, y, 0, 1), // vertical
+        getString(grid, x, y, -1, 1), // diagonal forwards
+        getString(grid, x, y, 1, 1), // diagonal backwards
+      ].count((str) => str === "XMAS" || str === "SAMX"),
     )
     .sum();
 }
@@ -27,12 +23,8 @@ export function day04b(input: string) {
     const diagonal1 = getString(grid, x - 1, y - 1, 1, 1, 3); // top-left to bottom-right
     const diagonal2 = getString(grid, x - 1, y + 1, 1, -1, 3); // bottom-left to top-right
 
-    // check they match, either forwards or backwards
-    const validMas1 = diagonal1 === "MAS" || diagonal1 === "SAM";
-    const validMas2 = diagonal2 === "MAS" || diagonal2 === "SAM";
-
-    // we need a MAS on both diagonals for a valid X-MAS
-    return validMas1 && validMas2;
+    // check they match, either forwards or backwards - we need a MAS on both diagonals for a valid X-MAS
+    return (diagonal1 === "MAS" || diagonal1 === "SAM") && (diagonal2 === "MAS" || diagonal2 === "SAM");
   });
 }
 
