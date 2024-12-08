24f3c6c02e27ae1a4aa3131bd5c3709712a6e033
jyelewis <jye.lewis@propelleraero.com.au>
2024-12-08 18:34:07 +1100

    Day 8 - clean up

--- 08/08.ts
+++ 08/08.ts
@@ -5,43 +5,41 @@ export function day08(input: string) {
   const grid = input.split("\n").map((line) => line.split(""));
 
   return {
-    partA: countAntinodes(grid, false),
-    partB: countAntinodes(grid, true),
+    partA: countAntiNodes(grid, false),
+    partB: countAntiNodes(grid, true),
   };
 }
 
-function countAntinodes(grid: string[][], includeHarmonics: boolean) {
-  const antinodes = new Set<string>(); // x,y
+function countAntiNodes(grid: string[][], includeHarmonics: boolean) {
+  const antiNodes = new Set<string>(); // x,y
 
-  range2D(grid).forEach(({ x: x1, y: y1 }) => {
-    // find antinodes surounding this node
-    const node = grid[y1][x1];
-    if (node === ".") return;
+  range2D(grid)
+    .filter(({ value }) => value !== ".")
+    .forEach(({ x: x1, y: y1, value: node1 }) =>
+      range2D(grid)
+        // find nodes with same frequency, excluding the current node
+        .filter(({ x: x2, y: y2, value: node2 }) => !(x1 === x2 && y1 === y2) && node1 === node2)
+        // calculate the antinodes, including harmonics
+        .forEach(({ x: x2, y: y2 }) => {
+          const dx = Math.abs(x2 - x1);
+          const dy = Math.abs(y2 - y1);
 
-    range2D(grid)
-      // find pair nodes, excluding the current node
-      .filter(({ x: x2, y: y2 }) => (x1 !== x2 || y1 !== y2) && grid[y2][x2] === node)
-      // calculate the antinodes, including harmonics
-      .forEach(({ x: x2, y: y2 }) => {
-        const dx = Math.abs(x2 - x1);
-        const dy = Math.abs(y2 - y1);
+          let i = includeHarmonics ? 0 : 1;
+          do {
+            // check for harmonics
+            const antiNodeX = x1 > x2 ? x1 + dx * i : x1 - dx * i;
+            const antiNodeY = y1 > y2 ? y1 + dy * i : y1 - dy * i;
 
-        let i = includeHarmonics ? 0 : 1;
-        do {
-          // check for harmonics
-          const antiNodeX = x1 > x2 ? x1 + dx * i : x1 - dx * i;
-          const antiNodeY = y1 > y2 ? y1 + dy * i : y1 - dy * i;
+            // we're out of bounds, give up
+            if (antiNodeX < 0 || antiNodeX >= grid[0].length || antiNodeY < 0 || antiNodeY >= grid.length) return;
 
-          // we're out of bounds, give up
-          if (antiNodeX < 0 || antiNodeX >= grid[0].length || antiNodeY < 0 || antiNodeY >= grid.length) return;
+            antiNodes.add(`${antiNodeX},${antiNodeY}`);
 
-          antinodes.add(`${antiNodeX},${antiNodeY}`);
+            // if we're looking for harmonics, we need to keep going until we hit the edge
+            i++;
+          } while (includeHarmonics);
+        }),
+    );
 
-          // if we're looking for harmonics, we need to keep going until we hit the edge
-          i++;
-        } while (includeHarmonics);
-      });
-  });
-
-  return antinodes.size;
+  return antiNodes.size;
 }
--- utilities/range2D.ts
+++ utilities/range2D.ts
@@ -1,15 +1,16 @@
 import { range } from "./range";
 
-export function range2D(arr: unknown[][]): Array<{ x: number; y: number }>;
-export function range2D(sizeX: number, sizeY: number): Array<{ x: number; y: number }>;
-export function range2D(sizeXOrArr: unknown[][] | number, sizeY?: number): Array<{ x: number; y: number }> {
+export function range2D<T>(arr: T[][]): Array<{ x: number; y: number; value: T }>;
+export function range2D(sizeX: number, sizeY: number): Array<{ x: number; y: number; value: never }>;
+export function range2D<T>(sizeXOrArr: T[][] | number, sizeY?: number): Array<{ x: number; y: number; value: T }> {
+  const array = Array.isArray(sizeXOrArr) ? sizeXOrArr : undefined;
   const sizeX = Array.isArray(sizeXOrArr) ? sizeXOrArr[0].length : sizeXOrArr;
   const realSizeY = Array.isArray(sizeXOrArr) ? sizeXOrArr.length : sizeY!;
 
-  const arr: Array<{ x: number; y: number }> = [];
+  const arr: Array<{ x: number; y: number; value: any }> = [];
   for (const y of range(realSizeY)) {
     for (const x of range(sizeX)) {
-      arr.push({ x, y });
+      arr.push({ x, y, value: array?.[y]?.[x] });
     }
   }
 
