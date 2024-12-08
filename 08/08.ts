2662d335b9c3c0d5120049289d0982feaf10bde3
jyelewis <jye.lewis@propelleraero.com.au>
2024-12-08 18:40:42 +1100

    Day 8 - clean up

--- 08/08.ts
+++ 08/08.ts
@@ -21,14 +21,10 @@ function countAntiNodes(grid: string[][], includeHarmonics: boolean) {
         .filter(({ x: x2, y: y2, value: node2 }) => !(x1 === x2 && y1 === y2) && node1 === node2)
         // calculate the antinodes, including harmonics
         .forEach(({ x: x2, y: y2 }) => {
-          const dx = Math.abs(x2 - x1);
-          const dy = Math.abs(y2 - y1);
-
           let i = includeHarmonics ? 0 : 1;
           do {
-            // check for harmonics
-            const antiNodeX = x1 > x2 ? x1 + dx * i : x1 - dx * i;
-            const antiNodeY = y1 > y2 ? y1 + dy * i : y1 - dy * i;
+            const antiNodeX = x1 + (x1 - x2) * i;
+            const antiNodeY = y1 + (y1 - y2) * i;
 
             // we're out of bounds, give up
             if (antiNodeX < 0 || antiNodeX >= grid[0].length || antiNodeY < 0 || antiNodeY >= grid.length) return;
