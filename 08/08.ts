17998d329a6d5bc4c00d282b49f1a170d48ce405
jyelewis <jye.lewis@propelleraero.com.au>
2024-12-08 18:45:41 +1100

    Day 8 - clean up

--- 08/08.ts
+++ 08/08.ts
@@ -13,26 +13,26 @@ export function day08(input: string) {
 function countAntiNodes(grid: string[][], includeHarmonics: boolean) {
   const antiNodes = new Set<string>(); // x,y
 
-  range2D(grid)
-    .filter(({ value }) => value !== ".")
-    .forEach(({ x: x1, y: y1, value: node1 }) =>
-      range2D(grid)
-        // find nodes with same frequency, excluding the current node
-        .filter(({ x: x2, y: y2, value: node2 }) => !(x1 === x2 && y1 === y2) && node1 === node2)
-        // calculate the antinodes, including harmonics
+  range2D(grid) // find harmonics for each tower in the grid
+    .filter(({ value }) => value !== ".") // remove empty spaces
+    .forEach(({ x: x1, y: y1, value: freq1 }) =>
+      range2D(grid) // find other towers with same frequency
+        .filter(({ x: x2, y: y2, value: freq2 }) => !(x1 === x2 && y1 === y2) && freq1 === freq2)
+        // calculate the anti-nodes, including harmonics, for each tower pair
         .forEach(({ x: x2, y: y2 }) => {
-          let i = includeHarmonics ? 0 : 1;
+          let harmonic = includeHarmonics ? 0 : 1; // only include self if looking for harmonics
           do {
-            const antiNodeX = x1 + (x1 - x2) * i;
-            const antiNodeY = y1 + (y1 - y2) * i;
+            // rise over run babyeeee
+            const antiNodeX = x1 + (x1 - x2) * harmonic;
+            const antiNodeY = y1 + (y1 - y2) * harmonic;
 
-            // we're out of bounds, give up
-            if (antiNodeX < 0 || antiNodeX >= grid[0].length || antiNodeY < 0 || antiNodeY >= grid.length) return;
+            if (antiNodeX < 0 || antiNodeX >= grid[0].length || antiNodeY < 0 || antiNodeY >= grid.length) return; // we're out of bounds, give up
 
+            // another anti-node found, add it to the set
             antiNodes.add(`${antiNodeX},${antiNodeY}`);
 
             // if we're looking for harmonics, we need to keep going until we hit the edge
-            i++;
+            harmonic += 1;
           } while (includeHarmonics);
         }),
     );
