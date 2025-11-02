800e218d1e6cdee75d256a590c513c160c9837cf
Jye Lewis <jye@jyelewis.com>
2025-11-03 09:47:30 +1100

    2024 - 12b

--- 12/12.test.ts
+++ 12/12.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from "node:test";
 import fs from "fs";
 import assert from "node:assert";
-import { day12a, day12b } from "./12";
+import { day12a, day12b, Region, sidesForRegion } from "./12";
 
 describe("day12", () => {
   const sampleInput = fs.readFileSync("12/example-input.txt").toString("utf-8");
@@ -19,11 +19,35 @@ describe("day12", () => {
 
   it("12b sample input", () => {
     const answer = day12b(sampleInput);
-    assert.equal(answer, 123);
+    assert.equal(answer, 1206);
   });
 
   it("12b input", () => {
     const answer = day12b(input);
-    assert.equal(answer, 123);
+    assert.equal(answer, 853588);
+  });
+
+  describe("sidesForRegion", () => {
+    it("I", () => {
+      const region: Region = {
+        plantType: "I",
+        cells: [
+          { x: 4, y: 0 },
+          { x: 5, y: 0 },
+          { x: 5, y: 1 },
+          { x: 4, y: 1 },
+        ],
+      };
+
+      const sides = sidesForRegion(region);
+      assert.equal(sides, 4);
+    });
+
+    it("C", () => {
+      const region: Region = { plantType: "C", cells: [{ x: 7, y: 4 }] };
+
+      const sides = sidesForRegion(region);
+      assert.equal(sides, 4);
+    });
   });
 });
