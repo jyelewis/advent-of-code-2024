031747bc446fe462f75f25f12c32b97fb287d285
Jye Lewis <jye@jyelewis.com>
2025-11-03 11:06:07 +1100

    Clean up

--- 12/12.test.ts
+++ 12/12.test.ts
@@ -1,30 +1,22 @@
 import { describe, it } from "node:test";
 import fs from "fs";
 import assert from "node:assert";
-import { day12a, day12b, Region, sidesForRegion } from "./12";
+import { day12, Region, sidesForRegion } from "./12";
 
 describe("day12", () => {
   const sampleInput = fs.readFileSync("12/example-input.txt").toString("utf-8");
   const input = fs.readFileSync("12/input.txt").toString("utf-8");
 
-  it("12a sample input", () => {
-    const answer = day12a(sampleInput);
-    assert.equal(answer, 1930);
+  it("12 sample input", () => {
+    const { partA, partB } = day12(sampleInput);
+    assert.equal(partA, 1930);
+    assert.equal(partB, 1206);
   });
 
-  it("12a input", () => {
-    const answer = day12a(input);
-    assert.equal(answer, 1396298);
-  });
-
-  it("12b sample input", () => {
-    const answer = day12b(sampleInput);
-    assert.equal(answer, 1206);
-  });
-
-  it("12b input", () => {
-    const answer = day12b(input);
-    assert.equal(answer, 853588);
+  it("12 input", () => {
+    const { partA, partB } = day12(input);
+    assert.equal(partA, 1396298);
+    assert.equal(partB, 853588);
   });
 
   describe("sidesForRegion", () => {
--- 12/12.ts
+++ 12/12.ts
@@ -1,5 +1,4 @@
 import { Pos, range2D } from "../utilities";
-import assert from "node:assert";
 
 export interface Region {
   plantType: string;
@@ -26,241 +25,104 @@ function perimeterForRegion(region: Region) {
     .sum();
 }
 
-export function day12a(input: string) {
-  const grid = input.split("\n").map((line) => line.split(""));
-
-  // find enclosed regions
-  const visitedCells = new Set<string>();
-  const regions: Region[] = [];
-
-  // gross, we mutate cells
-  const findRegion = (x: number, y: number, plantType: string, cells: Pos[]) => {
-    // out of bounds
-    if (x < 0 || y < 0 || x >= grid[0].length || y >= grid.length) {
-      return;
-    }
-
-    // not the right type
-    if (grid[y][x] !== plantType) {
-      return;
-    }
-
-    // already part of a region
-    if (visitedCells.has(`${x},${y}`)) {
-      return;
-    }
-
-    visitedCells.add(`${x},${y}`);
-    cells.push({ x, y });
-
-    findRegion(x + 1, y, plantType, cells);
-    findRegion(x - 1, y, plantType, cells);
-    findRegion(x, y + 1, plantType, cells);
-    findRegion(x, y - 1, plantType, cells);
-  };
-
-  range2D(grid).forEach(({ x, y, value: plantType }) => {
-    const cells: Pos[] = [];
-    findRegion(x, y, plantType, cells);
-    if (cells.length > 0) {
-      regions.push({ plantType, cells });
-    }
-  });
-
-  return regions.map((r) => r.cells.length * perimeterForRegion(r)).sum();
-}
-
 export function sidesForRegion(region: Region) {
-  // for each cell
+  // generate a list of lines representing that cells sides (complete lines)
+  // dedup sides
+  // count
   return region.cells
     .flatMap((cell) => {
       const cellWalls: Array<{
-        tag: string;
+        side: string;
         xStart: number;
         xEnd: number;
         yStart: number;
         yEnd: number;
       }> = [];
 
-      // TODO: this can be dramatically simplified
+      const directions = [
+        {
+          side: "top",
+          wall: { x: 0, y: -1 },
+          start: { x: -1, y: 0 },
+          end: { x: 1, y: 0 },
+        },
+        {
+          side: "bottom",
+          wall: { x: 0, y: 1 },
+          start: { x: -1, y: 0 },
+          end: { x: 1, y: 0 },
+        },
+        {
+          side: "left",
+          wall: { x: -1, y: 0 },
+          start: { x: 0, y: -1 },
+          end: { x: 0, y: 1 },
+        },
+        {
+          side: "right",
+          wall: { x: 1, y: 0 },
+          start: { x: 0, y: -1 },
+          end: { x: 0, y: 1 },
+        },
+      ];
+      for (const direction of directions) {
+        const isMissingCellAdjacent = !region.cells.some(
+          (c) => c.x === cell.x + direction.wall.x && c.y === cell.y + direction.wall.y,
+        );
+        if (!isMissingCellAdjacent) {
+          // no gap, no wall
+          continue;
+        }
 
-      // top side
-      const isMissingCellAbove = region.cells.some((c) => c.x === cell.x && c.y === cell.y - 1) === false;
-      if (isMissingCellAbove) {
         // there must be a wall above us!
         // how far out does it extend?
         let xStart = cell.x;
-        while (xStart >= 0) {
-          const isPrevHavingCell = region.cells.some((c) => c.x === xStart - 1 && c.y === cell.y);
-          const isPrevMissingCellAbove = region.cells.some((c) => c.x === xStart - 1 && c.y === cell.y - 1);
-
-          if (isPrevHavingCell && !isPrevMissingCellAbove) {
-            // we can extend our wall to the left
-            xStart--;
-
-            // and go again
-          } else {
-            // we've reached the left end of the wall
-            break;
-          }
-        }
-        assert(xStart >= 0);
-
-        let xEnd = cell.x;
-        while (xEnd <= 10_000) {
-          const isNextHavingCell = region.cells.some((c) => c.x === xEnd + 1 && c.y === cell.y);
-          const isNextMissingCellAbove = region.cells.some((c) => c.x === xEnd + 1 && c.y === cell.y - 1);
-          if (isNextHavingCell && !isNextMissingCellAbove) {
-            // we can extend our wall to the right
-            xEnd++;
-          } else {
-            // we've reached the right end of the wall
-            break;
-          }
-        }
-        assert(xEnd < 10_000);
-
-        cellWalls.push({
-          tag: "top",
-          xStart,
-          xEnd,
-          yStart: cell.y,
-          yEnd: cell.y,
-        });
-      }
-
-      // bottom side
-      const isMissingCellBelow = region.cells.some((c) => c.x === cell.x && c.y === cell.y + 1) === false;
-      if (isMissingCellBelow) {
-        // there must be a wall below us!
-        // how far out does it extend?
-        let xStart = cell.x;
-        while (xStart >= 0) {
-          const isPrevHavingCell = region.cells.some((c) => c.x === xStart - 1 && c.y === cell.y);
-          const isPrevMissingCellBelow = region.cells.some((c) => c.x === xStart - 1 && c.y === cell.y + 1);
-
-          if (isPrevHavingCell && !isPrevMissingCellBelow) {
-            // we can extend our wall to the left
-            xStart--;
-
-            // and go again
-          } else {
-            // we've reached the left end of the wall
-            break;
-          }
-        }
-        assert(xStart >= 0);
-
-        let xEnd = cell.x;
-        while (xEnd <= 10_000) {
-          const isNextHavingCell = region.cells.some((c) => c.x === xEnd + 1 && c.y === cell.y);
-          const isNextMissingCellBelow = region.cells.some((c) => c.x === xEnd + 1 && c.y === cell.y + 1);
-          if (isNextHavingCell && !isNextMissingCellBelow) {
-            // we can extend our wall to the right
-            xEnd++;
-          } else {
-            // we've reached the right end of the wall
-            break;
-          }
-        }
-        assert(xEnd < 10_000);
-
-        cellWalls.push({
-          tag: "bottom",
-          xStart,
-          xEnd,
-          yStart: cell.y + 1,
-          yEnd: cell.y + 1,
-        });
-      }
-
-      // left side
-      const isMissingCellLeft = region.cells.some((c) => c.x === cell.x - 1 && c.y === cell.y) === false;
-      if (isMissingCellLeft) {
-        // there must be a wall left of us!
-        // how far out does it extend?
         let yStart = cell.y;
-        while (yStart >= 0) {
-          const isAboveHavingCell = region.cells.some((c) => c.x === cell.x && c.y === yStart - 1);
-          const isAboveMissingCellLeft = region.cells.some((c) => c.x === cell.x - 1 && c.y === yStart - 1);
 
-          if (isAboveHavingCell && !isAboveMissingCellLeft) {
-            // we can extend our wall up
-            yStart--;
+        while (true) {
+          const isPrevHavingCell = region.cells.some(
+            (c) => c.x === xStart + direction.start.x && c.y === yStart + direction.start.y,
+          );
+          const isPrevMissingCellAdjacent = !region.cells.some(
+            (c) =>
+              c.x === xStart + direction.start.x + direction.wall.x &&
+              c.y === yStart + direction.start.y + direction.wall.y,
+          );
 
-            // and go again
-          } else {
-            // we've reached the left end of the wall
-            break;
-          }
-        }
-        assert(yStart >= 0);
-
-        let yEnd = cell.y;
-        while (yEnd <= 10_000) {
-          const isBelowHavingCell = region.cells.some((c) => c.x === cell.x && c.y === yEnd + 1);
-          const isBelowMissingCellLeft = region.cells.some((c) => c.x === cell.x - 1 && c.y === yEnd + 1);
-
-          if (isBelowHavingCell && !isBelowMissingCellLeft) {
-            // we can extend our wall dowm
-            yEnd++;
-          } else {
+          if (!isPrevHavingCell || !isPrevMissingCellAdjacent) {
             // we've reached the end of the wall
             break;
           }
+
+          xStart += direction.start.x;
+          yStart += direction.start.y;
         }
-        assert(yEnd < 10_000);
 
-        cellWalls.push({
-          tag: "left",
-          xStart: cell.x,
-          xEnd: cell.x,
-          yStart,
-          yEnd,
-        });
-      }
-
-      // right side
-      const isMissingCellRight = region.cells.some((c) => c.x === cell.x + 1 && c.y === cell.y) === false;
-      if (isMissingCellRight) {
-        // there must be a wall left of us!
-        // how far out does it extend?
-        let yStart = cell.y;
-        while (yStart >= 0) {
-          const isAboveHavingCell = region.cells.some((c) => c.x === cell.x && c.y === yStart - 1);
-          const isAboveMissingCellRight = region.cells.some((c) => c.x === cell.x + 1 && c.y === yStart - 1);
-
-          if (isAboveHavingCell && !isAboveMissingCellRight) {
-            // we can extend our wall up
-            yStart--;
-
-            // and go again
-          } else {
-            // we've reached the left end of the wall
-            break;
-          }
-        }
-        assert(yStart >= 0);
-
+        let xEnd = cell.x;
         let yEnd = cell.y;
-        while (yEnd <= 10_000) {
-          const isBelowHavingCell = region.cells.some((c) => c.x === cell.x && c.y === yEnd + 1);
-          const isBelowMissingCellRight = region.cells.some((c) => c.x === cell.x + 1 && c.y === yEnd + 1);
-
-          if (isBelowHavingCell && !isBelowMissingCellRight) {
-            // we can extend our wall dowm
-            yEnd++;
-          } else {
-            // we've reached the end of the wall
+        while (true) {
+          const isNextHavingCell = region.cells.some(
+            (c) => c.x === xEnd + direction.end.x && c.y === yEnd + direction.end.y,
+          );
+          const isNextMissingCellAdjacent = !region.cells.some(
+            (c) =>
+              c.x === xStart + direction.end.x + direction.wall.x &&
+              c.y === yStart + direction.end.y + direction.wall.y,
+          );
+          if (!isNextHavingCell || !isNextMissingCellAdjacent) {
+            // we've reached the end of our wall
             break;
           }
+
+          // continue extending the end of our wall
+          xEnd += direction.end.x;
+          yEnd += direction.end.y;
         }
-        assert(yEnd < 10_000);
 
         cellWalls.push({
-          tag: "right",
-          xStart: cell.x + 1,
-          xEnd: cell.x + 1,
+          side: direction.side,
+          xStart,
+          xEnd,
           yStart,
           yEnd,
         });
@@ -268,14 +130,10 @@ export function sidesForRegion(region: Region) {
 
       return cellWalls;
     })
-    .dedup(({ tag, xStart, xEnd, yStart, yEnd }) => `${tag}-${xStart},${yStart}-${xEnd},${yEnd}`).length;
-  // generate a list of lines representing that cells sides (complete lines)
-  // dedup sides
-  // count
+    .dedup(({ side, xStart, xEnd, yStart, yEnd }) => `${side}-${xStart},${yStart}-${xEnd},${yEnd}`).length;
 }
 
-// TODO: merge with a
-export function day12b(input: string) {
+export function day12(input: string) {
   const grid = input.split("\n").map((line) => line.split(""));
 
   // find enclosed regions
@@ -316,5 +174,8 @@ export function day12b(input: string) {
     }
   });
 
-  return regions.map((r) => r.cells.length * sidesForRegion(r)).sum();
+  return {
+    partA: regions.map((r) => r.cells.length * perimeterForRegion(r)).sum(),
+    partB: regions.map((r) => r.cells.length * sidesForRegion(r)).sum(),
+  };
 }
